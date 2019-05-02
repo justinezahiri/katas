bf418af5bcf93e26c2e77fa88cd0f53d3392ac1a
justinezahiri <justinezahiri@gmail.com>
2019-05-02 17:53:28 +0200

    century from year kata

--- 02_05Katas.js
+++ 02_05Katas.js
@@ -41,3 +41,16 @@ function solution(str) {
   //join() crée et renvoie une nouvelle chaîne de caractères en concaténant tous les éléments d'un array
   return str.split("").reverse().join("");
 }
+
+//Century From Year
+//The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc. Given a year, return the century it is in.
+function century(year) {
+  var century = 0;
+
+  for (var i = 0; i < year; i++) {
+    if (i % 100 == 0) {
+      century++;
+    }
+  }
+  return century;
+}

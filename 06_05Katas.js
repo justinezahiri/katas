de94c43637031701cef6b52c3d6bb3d80bde3e45
justinezahiri <justinezahiri@gmail.com>
2019-05-06 22:59:33 +0200

    reversed sequence kata

--- 06_05Katas.js
+++ 06_05Katas.js
@@ -10,3 +10,14 @@ function doubleChar(str) {
     }).join('')
   );
 }
+
+//Reversed sequence
+/* Get the number n (n>0) to return the reversed sequence from n to 1.
+Example : n=5 >> [5,4,3,2,1] */
+const reverseSeq = n => {
+  let arr = [];
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+};

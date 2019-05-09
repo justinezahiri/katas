a15695714a5706ddac13c15f016b0abcb8024962
justinezahiri <justinezahiri@gmail.com>
2019-05-09 16:15:27 +0200

    accum kata

--- 09_05Katas.js
+++ 09_05Katas.js
@@ -5,3 +5,20 @@ var stringToNumber = function (str) {
 
   return null;
 }
+
+// function accum: 
+/*examples: accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+*/
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
+//or with for loop
+function accum(str) {
+  var letters = str.split('');
+  var result = [];
+  for (var i = 0; i < letters.length; i++) {
+    result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+  return result.join('-');
+}

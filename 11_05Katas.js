b52b53bf9a33d1e10196025c303aaf432166b266
justinezahiri <justinezahiri@gmail.com>
2019-05-11 14:04:19 +0200

    vowels count kata

--- 11_05Katas.js
+++ 11_05Katas.js
@@ -16,3 +16,48 @@ function getMiddle(s) {
 
   return s.substring(position, position + length)
 }
+//or
+function getMiddle(s) {
+  var middle = s.length / 2;
+  return (s.length % 2) ?
+    s.charAt(Math.floor(middle)) :
+    s.slice(middle - 1, middle + 1);
+}
+
+//Return the number (count) of vowels in the given string.
+function getCount(str) {
+  var vowelsCount = 0;
+
+  //turn the input into a string
+  var string = str.toString();
+
+  //loop through the string
+  for (var i = 0; i <= string.length - 1; i++) {
+
+    //if a vowel, add to vowel count
+    if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+      vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
+//or
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a", "e", "i", "o", "u"];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
+        vowelsCount++;
+      }
+    }
+  }
+  return vowelsCount;
+}
+//or using split and filter
+function getCount(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  return str.split('').filter(letter => {
+    return vowels.includes(letter) ? true : false;
+  }).length;
+}

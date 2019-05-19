a9bc98a42931fb0c0e3c5cb403651c35f40039af
justinezahiri <justinezahiri@gmail.com>
2019-05-19 22:45:31 +0200

    square every digit kata

---
+++ 19_05Katas.js
@@ -0,0 +1,7 @@
+//square every digit of a number
+//For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+function squareDigits(num) {
+  return Number(('' + num).split('').map(function (val) {
+    return val * val;
+  }).join(''));
+}

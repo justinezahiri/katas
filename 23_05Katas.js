d11763a4b1fe671d46e1c9eafbf444bf7013c63a
justinezahiri <justinezahiri@gmail.com>
2019-05-26 18:03:01 +0200

    reversed words kata

---
+++ 23_05Katas.js
@@ -0,0 +1,27 @@
+//Your order, please
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+ */
+function order(words) {
+
+  return words.split(' ').sort(function (a, b) {
+    return a.match(/\d/) - b.match(/\d/);
+  }).join(' ');
+}
+//or
+function order(words) {
+  var array = words.split(' ');
+  var sortedArray = [];
+  for (i = 0; i <= array.length; i++) {
+    for (j = 0; j < array.length; j++) {
+      if (array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}

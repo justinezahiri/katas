261badefdf3a5bc3cb3e897eae3096753e75961d
justinezahiri <justinezahiri@gmail.com>
2019-06-11 22:36:04 +0200

    extact domain name from an url kata

--- 11_06Katas.js
+++ 11_06Katas.js
@@ -4,3 +4,16 @@ var isSquare = function (n) {
   //using Math.sqrt
   return Math.sqrt(n) % 1 === 0;
 }
+
+//extract the domain name from an url
+function domainName(url) {
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+//or
+function domainName(url) {
+  return url.replace(/(https?:\/\/)?(www\.)?/, '').split('.')[0]
+}
+//or
+function domainName(url) {
+  return url.replace(/.+\/\/|www.|\..+/g, '')
+}

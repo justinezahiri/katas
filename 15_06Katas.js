12f454d102036826e082f82a62d43ac5eb179f8d
justinezahiri <justinezahiri@gmail.com>
2019-06-16 23:35:09 +0200

    count of positives kata WIP

--- 15_06Katas.js
+++ 15_06Katas.js
@@ -8,3 +8,21 @@ function joinStrings(string1, string2) {
 }
 //or
 const joinStrings = (string1, string2) => `${string1} ${string2}`;
+
+//Count of positives / sum of negatives
+/*
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+If the input array is empty or null, return an empty array.
+*/
+function countPositivesSumNegatives(input) {
+  if (input === [] || null) {
+    return [];
+  }
+  //iterate over input array
+  input.map
+  //create an array to push count of positive numbers
+  //create an array to push sum of negative numbers
+  //test if array element is a positive number 
+  //test if array element is a negative number
+  return ?;
+}

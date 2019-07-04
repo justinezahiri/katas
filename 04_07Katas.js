913476b339db89dc4df395eea7dbfea4f3d33968
justinezahiri <justinezahiri@gmail.com>
2019-07-04 13:14:07 +0100

    rock paper scissors

--- 04_07Katas.js
+++ 04_07Katas.js
@@ -63,3 +63,29 @@ function rps(p1, p2) {
   if (x > y) return 'Player 1 won!'
   else return 'Player 2 won!';
 }
+//or
+// Makes the game extensible (in case we need lizards and spock later)
+const hands = {
+  "rock": "paper",
+  "scissors": "rock",
+  "paper": "scissors"
+}
+
+//use a rest param for the players,so I can use indexOf to "find" a winner, rather than logic-out a winner
+
+const rps = (...players) => {
+  let result = "Draw!"
+
+  //don't do anything if there's a draw
+  if (players[0] !== players[1]) {
+    let winner;
+
+    for (let hand in hands) {
+      // winner logic: the "hand" was played in the players, and we **don't** have the thing that could beat it
+      if (players.indexOf(hand) !== -1 && players.indexOf(hands[hand]) == -1) winner = players.indexOf(hand)
+    }
+
+    result = `Player ${winner+1} won!`
+  }
+  return result;
+};
